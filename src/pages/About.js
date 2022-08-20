d9a4c082e4844e8b6f6514a78e6a247919c89441
unknown <cu.18bcs6591@gmail.com>
2022-08-20 17:21:53 +0530

    intial commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis aliquam
+        error unde? Praesentium, doloribus, atque nemo explicabo, at repudiandae
+        animi voluptatibus velit eius debitis quia. Aliquam explicabo provident
+        maiores tempore.
+      </p>
+    </section>
+  );
+};
+
+export default About;
